3ede31b6b9c597e04a35664bdfa0df09f8a2e5c3
alfredochola <ochalfie@gmail.com>
2024-02-07 00:19:50 +0300

    major changes from client

--- _site/js/script.js
+++ _site/js/script.js
@@ -1,36 +1,40 @@
 function contactus() {
   Swal.fire({
-    title: "Contact Us",
     html: `
-        <div>
+        <div> <h1>Contact Us <button class="btn-lg btn-danger rounded-circle border-0" onclick="closePopup()"> &times;
+        </button></h1>
           
-          <div class="w-100 bg-light text-center"><img src="images/10.jpg" alt="Your Photo" class="w-50 text-center" /></div>
+          <div class="w-100 bg-light text-center"><img src="images/BOARDS.jpg" alt="Your Photo" class="w-100 text-center" /></div>
         
          <div class="my-3 text-center">
           
          <p class="py-3"> <button class="btn-lg btn-green border-0" onclick="whatsappUs()">
             <i class="bi bi-whatsapp"></i>  WhatsApp Us 
           </button>
-          <button class="btn-lg btn-dark border-0" onclick="closePopup()">Close</button>
+         
 </p>
 
           <p class="pb-5">
-          <button class="btn btn-primary rounded rounded-5" onclick="window.location.href='https://twitter.com/JALivingSpaces'">
+          <button class="btn btn-dark rounded rounded-5" onclick="window.location.href='https://twitter.com/JALivingSpaces'">
             <i class="bi bi-twitter"></i></button>
-            <button class="btn btn-primary rounded rounded-5" onclick="window.location.href='https://facebook.com/jalivingspaces'">
+            <button class="btn btn-dark rounded rounded-5" onclick="window.location.href='https://facebook.com/jalivingspaces'">
             <i class="bi bi-facebook"></i></button>
-            <button class="btn btn-primary rounded rounded-5" onclick="window.location.href='https://www.linkedin.com/company/j-a-design-associates/'">
+            <button class="btn btn-dark rounded rounded-5" onclick="window.location.href='https://www.linkedin.com/company/j-a-design-associates/'">
             <i class="bi bi-linkedin"></i></button>
-            <button class="btn btn-warning rounded rounded-5" onclick="window.location.href='https://www.instagram.com/jalivingspaces?igsh=MWlsNGRpNGhydTZ5YQ=='">
-            <i class="bi bi-instagram"></i></button>
+            <button class="btn btn-dark rounded rounded-5" onclick="window.location.href='https://www.instagram.com/jalivingspaces?igsh=MWlsNGRpNGhydTZ5YQ=='">
+            <i class="bi bi-instagram"></i></button></br><br>
+            <span class="p-1 w-100 bg-light">@jalivingspaces</span>
         </p>
-        <p class="p-1 bg-light">@jalivingspaces</p>
+     
         <p>
-          <span class="text-dark">+256 393 2471 59</span>
+          <a class="text-dark" href="tel:+256 393 2471 59">+256 393 2471 59</a>
           </p>
           <p>
-           <span class="text-dark">info@jalivingspaces.com</span>
+           <a class="text-dark" href="mailto:info@jalivingspaces.com ">info@jalivingspaces.com</a>
           </p>
+          <p>
+          <a class="text-dark" href="https://maps.app.goo.gl/7WzqebzwJ9Zr5no59?g_st=ic">  IJ Complex - Plot 1482, Mbogo Road - Kiwatule</a>
+         </p>
           </div>
 
         </div>
@@ -56,22 +60,25 @@ function getprojects() {
 </p>
 
           <p class="pb-5">
-          <button class="btn btn-primary rounded rounded-5" onclick="window.location.href='https://twitter.com/JALivingSpaces'">
+          <button class="btn btn-dark rounded rounded-5" onclick="window.location.href='https://twitter.com/JALivingSpaces'">
             <i class="bi bi-twitter"></i></button>
-            <button class="btn btn-primary rounded rounded-5" onclick="window.location.href='https://facebook.com/jalivingspaces'">
+            <button class="btn btn-dark rounded rounded-5" onclick="window.location.href='https://facebook.com/jalivingspaces'">
             <i class="bi bi-facebook"></i></button>
-            <button class="btn btn-primary rounded rounded-5" onclick="window.location.href='https://www.linkedin.com/company/j-a-design-associates/'">
+            <button class="btn btn-dark rounded rounded-5" onclick="window.location.href='https://www.linkedin.com/company/j-a-design-associates/'">
             <i class="bi bi-linkedin"></i></button>
-            <button class="btn btn-warning rounded rounded-5" onclick="window.location.href='https://www.instagram.com/jalivingspaces?igsh=MWlsNGRpNGhydTZ5YQ=='">
+            <button class="btn btn-dark rounded rounded-5" onclick="window.location.href='https://www.instagram.com/jalivingspaces?igsh=MWlsNGRpNGhydTZ5YQ=='">
             <i class="bi bi-instagram"></i></button>
+          
         </p>
-        <p class="p-1 bg-light">@jalivingspaces</p>
+        
         <p>
           <span class="text-dark">+256 393 2471 59</span>
           </p>
           <p>
            <span class="text-dark">info@jalivingspaces.com</span>
           </p>
+        
+        
           </div>
 
         </div>
@@ -87,5 +94,5 @@ function closePopup() {
 
 function whatsappUs() {
   window.location.href =
-    "https://wa.me/+256393247159?text=Hello JA Living Spaces, ...: ";
+    "https://wa.me/+256393247159?text=Hello J.A Living Spaces! ";
 }
